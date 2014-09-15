bce0c467bcf79ee2e0543661ca178f74d687f3ba
toddwseattle <todd@warrenfamily.org>
2014-09-15 09:46:31 +0000

    all tests past but akanname yet o be defined

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -4,9 +4,21 @@ var myApp=angular.module('myApp.view1', ['ngRoute']);
 
 
 
-myApp.controller('BirthdayCtrl', function($scope) {
+myApp.controller('BirthdayCtrl', function($scope,$http) {
 	var initbday=new Date(Date.now());
-
+	$http({method: 'GET', url: 'https://graph.facebook.com/me'}).
+	    success(function(data, status, headers, config) {
+	      // this callback will be called asynchronously
+	      // when the response is available
+	      console.log("success");
+	      console.log("data:"+data.toString());
+	    }).
+	    error(function(data, status, headers, config) {
+	      // called asynchronously if an error occurs
+	      // or server returns response with an error status.
+	      console.log("failure");
+	      console.log("Status"+status.toString());
+	    });
 	$scope.birthdaytext=initbday.toDateString();
 	$scope.genderinput="Male";
 	$scope.calculatebdayfromtext = function(birthdaytext)
